679a3b71721f127c59b56d338bf9a3a33f2ca8c3
Juan Abello <juandiegoabello@gmail.com>
2018-01-25 09:55:45 -0600

    Update bq-load-partition.sh

--- bq-load-partition.sh
+++ bq-load-partition.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#Edit the values of PROJECT, DATASET, PARTITIONED_TABLE, SOURCE_TABLE and SOURCE_TABLE_DATE
+
 now=`date +"%Y%m%d" -d "10/31/2017"`
 end=`date +"%Y%m%d" -d "11/30/2017"`
 
@@ -9,14 +11,10 @@ do
         now_two=`date +"%Y,%m,%d" -d "$now"`; 
         echo $now
         bq --nosync query --use_legacy_sql=false --allow_large_results --replace \
-		--noflatten_results --destination_table 'my_dataset.my_partitioned_table$'"$now"'' \
+		--noflatten_results --destination_table '[DATASET].[PARTITIONED_TABLE]$'"$now"'' \
 		'select
-		a,
-		e,
-		i,
-		o,
-		u
-		from `my_project.my_dataset.vowels` 
-		where TIMESTAMP_TRUNC(TIMESTAMP(vowel_date), DAY)=TIMESTAMP(DATE('"$now_two"'));'
+		*
+		from `[PROJECT].[DATASET].[SOURCE_TABLE]` 
+		where TIMESTAMP_TRUNC(TIMESTAMP([SOURCE_TABLE_DATE]), DAY)=TIMESTAMP(DATE('"$now_two"'));'
 		sleep 0.6
 done  
