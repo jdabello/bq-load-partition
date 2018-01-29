0e8c6118fac7af313b4c4c3e09c0b5070fa0785b
Juan Abello <jdabello@google.com>
2018-01-29 10:07:39 -0600

    Added arguments

--- bq-delete-partitions.sh
+++ bq-delete-partitions.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
-#Edit the values of DATASET, PARTITIONED_TABLE
 
-now=`date +"%Y%m%d" -d "10/31/2017"`
-end=`date +"%Y%m%d" -d "11/30/2017"`
+#e.g. bq-delete-partitions.sh partitioned_dataset partitioned_table 10/31/2017 11/30/2017
+
+partitioned_dataset=$1
+partitioned_table=$2
+from=$3 #MM/DD/YYYY
+to=$4 #MM/DD/YYYY
+
+now=`date +"%Y%m%d" -d "${from}"` 
+end=`date +"%Y%m%d" -d "${to}"`
 
 while [ "$now" != "$end" ] ; 
 do 
         now=`date +"%Y%m%d" -d "$now - 1 day"`; 
         now_two=`date +"%Y,%m,%d" -d "$now"`; 
         echo $now
-        bq rm -f '[DATASET].[PARTITIONED_TABLE]$'"$now"''
+        bq rm -f ''"${partitioned_dataset}"'.'"${partitioned_table}"'$'"${now}"''
 		sleep 0.6
 done  
