9c9a97dc9acd5bc606c9a5b5ade170138f8fe26d
Juan Abello <jdabello@google.com>
2018-01-29 09:54:03 -0600

    Added arguments project, source_dataset, source_table, partitioned_dataset, partitioned_table, source_table_date_field from and to

--- bq-load-partition.sh
+++ bq-load-partition.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
 
-#Edit the values of PROJECT, DATASET, PARTITIONED_TABLE, SOURCE_TABLE and SOURCE_TABLE_DATE
+#e.g. bq-load-partition.sh project-id source_dataset source_table partitioned_dataset partitioned_table source_table_date_field 10/31/2017 11/30/2017
 
-now=`date +"%Y%m%d" -d "10/31/2017"`
-end=`date +"%Y%m%d" -d "11/30/2017"`
+#Read parameters
+project=$1
+source_dataset=$2
+source_table=$3
+partitioned_dataset=$4
+partitioned_table=$5
+source_table_date_field=$6
+from=$7 #MM/DD/YYYY
+to=$8 #MM/DD/YYYY
+source_path="${project}.${source_dataset}.${source_table}"
+
+now=`date +"%Y%m%d" -d "${7}"` 
+end=`date +"%Y%m%d" -d "${8}"`
+
 
 while [ "$now" != "$end" ] ; 
 do 
         now=`date +"%Y%m%d" -d "$now + 1 day"`; 
         now_two=`date +"%Y,%m,%d" -d "$now"`; 
-        echo $now
+        partition="${partitioned_dataset}.${partitioned_table}\$${now}"
+        echo ${partition}
         bq --nosync query --use_legacy_sql=false --allow_large_results --replace \
-		--noflatten_results --destination_table '[DATASET].[PARTITIONED_TABLE]$'"$now"'' \
+		--noflatten_results --destination_table "'${partition}'" \
 		'select
 		*
-		from `[PROJECT].[DATASET].[SOURCE_TABLE]` 
-		where TIMESTAMP_TRUNC(TIMESTAMP([SOURCE_TABLE_DATE]), DAY)=TIMESTAMP(DATE('"$now_two"'));'
+		from `'"${source_path}"'` 
+		where TIMESTAMP_TRUNC(TIMESTAMP(['"${source_table_date_field}"']), DAY)=TIMESTAMP(DATE('"$now_two"'));'
 		sleep 0.6
 done  
